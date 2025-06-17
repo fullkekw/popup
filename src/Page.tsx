8c96d530f8485f5fc56ef56c5e3be56706d69ec3
Mykhailo Unniiiverse <unniiiverse@gmail.com>
2025-06-17 13:21:19 +0300

    fix: user select

--- src/Page.tsx
+++ src/Page.tsx
@@ -35,7 +35,7 @@ const Page: React.FC = () => {
 
           <div>
             <div>
-              <PopupWindow className="popup-1 w-[400px] h-[450px] bg-white" id={popupId1} settings={{ preventStateChange: true }} isOpen>
+              <PopupWindow className="popup-1 w-[400px] h-[450px] bg-white" id={popupId1} isOpen>
                 Hello
 
                 <PopupButton popupId={popupId1}>
